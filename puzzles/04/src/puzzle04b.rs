ed3e062a8deba0530c171833e4baefdd23d1985a
Drew Zemke <drew.zemke@gmail.com>
2024-12-05 22:20:03 -0800

    puzzle04: part b

--- puzzles/04/src/puzzle04b.rs
+++ puzzles/04/src/puzzle04b.rs
@@ -1,13 +1,28 @@
+use crate::Grid;
 use common::PuzzlePart;
 
 pub struct Puzzle04b {}
 
+const PATTERNS: [&str; 4] = [
+    "M.M\n.A.\nS.S",
+    "S.S\n.A.\nM.M",
+    "S.M\n.A.\nS.M",
+    "M.S\n.A.\nM.S",
+];
+
 impl PuzzlePart for Puzzle04b {
     fn description() -> &'static str {
-        "Puzzle 04 Part B"
+        "Count the number of times X's of 'MAS' appear in a grid."
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        let grid = Grid::parse(input);
+
+        PATTERNS
+            .into_iter()
+            .map(Grid::parse)
+            .map(|pattern| grid.count_matches(&pattern))
+            .sum::<usize>()
+            .to_string()
     }
 }
