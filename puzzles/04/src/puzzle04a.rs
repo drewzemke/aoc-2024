573a379293949d6ecb7faf98296dd21714c0f97a
Drew Zemke <drew.zemke@gmail.com>
2024-12-05 20:21:35 -0800

    puzzle04: part a again, but much simpler

--- puzzles/04/src/puzzle04a.rs
+++ puzzles/04/src/puzzle04a.rs
@@ -1,8 +1,18 @@
+use crate::Grid;
 use common::PuzzlePart;
 
-use crate::Grid;
-
 pub struct Puzzle04a {}
+
+const PATTERNS: [&str; 8] = [
+    "XMAS",
+    "SAMX",
+    "X\nM\nA\nS",
+    "S\nA\nM\nX",
+    "X...\n.M..\n..A.\n...S",
+    "S...\n.A..\n..M.\n...X",
+    "...X\n..M.\n.A..\nS...",
+    "...S\n..A.\n.M..\nX...",
+];
 
 impl PuzzlePart for Puzzle04a {
     fn description() -> &'static str {
@@ -10,6 +20,13 @@ impl PuzzlePart for Puzzle04a {
     }
 
     fn solve(input: &str) -> String {
-        Grid::parse(input).find_xmases().len().to_string()
+        let grid = Grid::parse(input);
+
+        PATTERNS
+            .into_iter()
+            .map(Grid::parse)
+            .map(|pattern| grid.count_matches(&pattern))
+            .sum::<usize>()
+            .to_string()
     }
 }
