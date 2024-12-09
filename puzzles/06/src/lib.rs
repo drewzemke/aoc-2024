dd6e1e190e5b8bce2f39594a699f64ebd38016c3
Drew Zemke <drew.zemke@gmail.com>
2024-12-08 23:00:25 -0800

    refactor: extract a `Point` module into common

--- puzzles/06/src/lib.rs
+++ puzzles/06/src/lib.rs
@@ -1,6 +1,5 @@
+use common::{grid::Grid, point::Point};
 use std::collections::HashSet;
-
-use common::grid::Grid;
 
 pub mod puzzle06a;
 pub mod puzzle06b;
@@ -41,13 +40,14 @@ impl Dir {
         }
     }
 
-    pub fn step(&self) -> (i64, i64) {
+    pub fn step(&self) -> Point {
         match self {
             Dir::North => (-1, 0),
             Dir::South => (1, 0),
             Dir::East => (0, 1),
             Dir::West => (0, -1),
         }
+        .into()
     }
 }
 
@@ -63,7 +63,7 @@ impl std::ops::Deref for GuardGrid {
 }
 
 impl GuardGrid {
-    pub fn parse(input: &str) -> (Self, (i64, i64), Dir) {
+    pub fn parse(input: &str) -> (Self, Point, Dir) {
         let grid = Grid::<Tile>::parse(input);
 
         // find the starting tile
@@ -83,25 +83,25 @@ impl GuardGrid {
 
         (
             GuardGrid(grid),
-            (row_idx as i64, col_idx as i64),
+            (row_idx as i64, col_idx as i64).into(),
             Dir::North,
         )
     }
 
-    fn walk(&self, start: (i64, i64), dir: Dir) -> impl Iterator<Item = ((i64, i64), Dir)> + '_ {
+    fn walk(&self, start: Point, dir: Dir) -> impl Iterator<Item = (Point, Dir)> + '_ {
         GuardGridWalker::new(self, start, dir)
     }
 
     /// walk the grid from the starting point/direction, turning right
     /// at every obstacle, until we exit the grid. return all of the
     /// points we walked over
-    pub fn walked_points(&self, start: (i64, i64), dir: Dir) -> HashSet<(i64, i64)> {
+    pub fn walked_points(&self, start: Point, dir: Dir) -> HashSet<Point> {
         self.walk(start, dir).map(|(pt, _)| pt).collect()
     }
 
     /// determines if a walked path through the grid will loop or not
-    fn will_it_loop(&self, start: (i64, i64), dir: Dir) -> bool {
-        let mut visited: HashSet<((i64, i64), Dir)> = HashSet::new();
+    fn will_it_loop(&self, start: Point, dir: Dir) -> bool {
+        let mut visited: HashSet<(Point, Dir)> = HashSet::new();
 
         for (pt, dir) in self.walk(start, dir) {
             if visited.contains(&(pt, dir)) {
@@ -114,14 +114,14 @@ impl GuardGrid {
         false
     }
 
-    fn put(&mut self, that: Tile, here: (i64, i64)) {
-        self.0 .0[here.0 as usize][here.1 as usize] = that;
+    fn put(&mut self, that: Tile, here: Point) {
+        self.0 .0[here.row as usize][here.col as usize] = that;
     }
 
     /// computes all obstacles that, if added (individually) to the grid,
     /// would cause the guard to get stuck in a loop
-    pub fn loop_causing_obstacles(&self, start: (i64, i64), dir: Dir) -> usize {
-        let mut obstacles: HashSet<(i64, i64)> = HashSet::new();
+    pub fn loop_causing_obstacles(&self, start: Point, dir: Dir) -> usize {
+        let mut obstacles: HashSet<Point> = HashSet::new();
 
         // used to test for looping after placing an obstacle
         let mut base_grid = self.clone();
@@ -147,12 +147,12 @@ impl GuardGrid {
 
 struct GuardGridWalker<'a> {
     grid: &'a GuardGrid,
-    pt: (i64, i64),
+    pt: Point,
     dir: Dir,
 }
 
 impl<'a> GuardGridWalker<'a> {
-    fn new(grid: &'a GuardGrid, start: (i64, i64), dir: Dir) -> Self {
+    fn new(grid: &'a GuardGrid, start: Point, dir: Dir) -> Self {
         Self {
             grid,
             pt: start,
@@ -162,7 +162,7 @@ impl<'a> GuardGridWalker<'a> {
 }
 
 impl<'a> Iterator for GuardGridWalker<'a> {
-    type Item = ((i64, i64), Dir);
+    type Item = (Point, Dir);
 
     fn next(&mut self) -> Option<Self::Item> {
         // determine the to-be-yielded point before updating
@@ -172,14 +172,12 @@ impl<'a> Iterator for GuardGridWalker<'a> {
             None
         };
 
-        let step = self.dir.step();
-        let mut next_pt = (self.pt.0 + step.0, self.pt.1 + step.1);
+        let mut next_pt = self.pt + self.dir.step();
 
         // turn right until we're not facing an obstacle
         while self.grid.contains(next_pt) && *self.grid.at_unchecked(next_pt) == Tile::Obstacle {
             self.dir.turn_right();
-            let step = self.dir.step();
-            next_pt = (self.pt.0 + step.0, self.pt.1 + step.1);
+            next_pt = self.pt + self.dir.step();
         }
 
         // advance
