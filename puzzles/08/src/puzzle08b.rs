04fe829dab8123a7d1f429febed95a308397c700
Drew Zemke <drew.zemke@gmail.com>
2024-12-08 20:17:30 -0800

    puzzle08: part b

--- puzzles/08/src/puzzle08b.rs
+++ puzzles/08/src/puzzle08b.rs
@@ -1,13 +1,16 @@
+use crate::AntennaGrid;
 use common::puzzle::PuzzlePart;
 
 pub struct Puzzle08b {}
 
 impl PuzzlePart for Puzzle08b {
     fn description() -> &'static str {
-        "Puzzle 08 Part B"
+        "Count the number of 'general antinodes' formed by pairs of antennae within a grid"
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        let grid = AntennaGrid::parse(input);
+
+        grid.all_general_antinodes().len().to_string()
     }
 }
