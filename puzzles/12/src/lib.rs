3c3b95684e7a7fc28d2cad8f0c6a63fdc3e019e3
Drew Zemke <drew.zemke@gmail.com>
2024-12-13 21:18:41 -0800

    puzzle12: part b

--- puzzles/12/src/lib.rs
+++ puzzles/12/src/lib.rs
@@ -133,6 +133,100 @@ impl Region {
             }
         }
     }
+
+    // count sides by counting corners
+    pub fn num_sides(&self) -> usize {
+        // iterate over a grid that contains this region, along with some padding
+        let rows = self.points.iter().map(|p| p.row);
+        let cols = self.points.iter().map(|p| p.col);
+        let min_row = rows.clone().min().unwrap();
+        let max_row = rows.max().unwrap() + 1;
+        let min_col = cols.clone().min().unwrap() - 1;
+        let max_col = cols.max().unwrap() + 1;
+
+        let mut sides = 0;
+
+        for row in min_row..=max_row {
+            for col in min_col..=max_col {
+                // look at the points around each point in order to figure out if there's a corner here
+                let here = self.points.contains(&(row, col).into());
+                let above = self.points.contains(&(row - 1, col).into());
+                let left = self.points.contains(&(row, col - 1).into());
+                let above_left = self.points.contains(&(row - 1, col - 1).into());
+
+                //  _
+                // _x
+                //  ^
+                let outer_top_left_corner = !above && !left && here;
+
+                // _
+                // x_
+                //  ^
+                let outer_top_right_corner = !above_left && left && !here;
+
+                // x_
+                // _
+                //  ^
+                let outer_bottom_right_corner = above_left && !above && !left;
+
+                // _x
+                //  _
+                //  ^
+                let outer_bottom_left_corner = !above_left && above && !here;
+
+                //  x
+                // x_
+                //  ^
+                let inner_top_left_corner = left && above && !here;
+
+                // x
+                // _x
+                //  ^
+                let inner_top_right_corner = above_left && !left && here;
+
+                // _x
+                // x
+                //  ^
+                let inner_bottom_right_corner = !above_left && above && left;
+
+                // x_
+                //  x
+                //  ^
+                let inner_bottom_left_corner = above_left && !above && here;
+
+                if outer_top_left_corner
+                    || outer_top_right_corner
+                    || outer_bottom_right_corner
+                    || outer_bottom_left_corner
+                    || inner_top_left_corner
+                    || inner_top_right_corner
+                    || inner_bottom_right_corner
+                    || inner_bottom_left_corner
+                {
+                    sides += 1;
+                }
+
+                // these two patterns are two corners at the same point,
+                // so we need to double count them:
+                //
+                // _x
+                // x_
+                //  ^
+                let double_corner1 = !above_left && above && left && !here;
+
+                // x_
+                // _x
+                //  ^
+                let double_corner2 = above_left && !above && !left && here;
+
+                if double_corner1 || double_corner2 {
+                    sides += 1;
+                }
+            }
+        }
+
+        sides
+    }
 }
 
 #[cfg(test)]
@@ -157,5 +251,60 @@ mod region_tests {
         };
 
         assert_eq!(region.perimeter(), 8);
+    }
+
+    #[test]
+    fn should_compute_num_sides() {
+        // x
+        //
+        let region = Region {
+            char: 'A',
+            points: vec![(10, 29).into()],
+        };
+
+        assert_eq!(region.num_sides(), 4);
+
+        // xx
+        //  x
+        let region = Region {
+            char: 'A',
+            points: vec![(2, 2).into(), (2, 3).into(), (3, 3).into()],
+        };
+
+        assert_eq!(region.num_sides(), 6);
+
+        //  x
+        // xxx
+        //  x
+        let region = Region {
+            char: 'A',
+            points: vec![
+                (0, 1).into(),
+                (1, 0).into(),
+                (1, 1).into(),
+                (1, 2).into(),
+                (2, 1).into(),
+            ],
+        };
+        assert_eq!(region.num_sides(), 12);
+
+        // xxx
+        // x x
+        // xx
+        let region = Region {
+            char: 'A',
+            points: vec![
+                (0, 0).into(),
+                (0, 1).into(),
+                (0, 2).into(),
+                (1, 0).into(),
+                (1, 2).into(),
+                (2, 0).into(),
+                (2, 1).into(),
+            ],
+        };
+
+        // 6 outside + 4 inside
+        assert_eq!(region.num_sides(), 10);
     }
 }
--- puzzles/12/src/puzzle12b.rs
+++ puzzles/12/src/puzzle12b.rs
@@ -1,13 +1,19 @@
+use crate::GardenGrid;
 use common::puzzle::PuzzlePart;
 
 pub struct Puzzle12b {}
 
 impl PuzzlePart for Puzzle12b {
     fn description() -> &'static str {
-        "Puzzle 12 Part B"
+        "Find the sum of the 'prices' (area * number of sides) of a bunch of regions of a grid."
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        GardenGrid::parse(input)
+            .regions()
+            .iter()
+            .map(|r| r.area() * r.num_sides())
+            .sum::<usize>()
+            .to_string()
     }
 }
