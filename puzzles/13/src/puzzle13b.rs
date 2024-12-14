9aef83f50b15976593736562a0efa128cfb12899
Drew Zemke <drew.zemke@gmail.com>
2024-12-14 00:29:04 -0800

    puzzle13: part b

--- puzzles/13/src/puzzle13b.rs
+++ puzzles/13/src/puzzle13b.rs
@@ -1,13 +1,28 @@
 use common::puzzle::PuzzlePart;
 
+use crate::ClawMachine;
+
 pub struct Puzzle13b {}
 
 impl PuzzlePart for Puzzle13b {
     fn description() -> &'static str {
-        "Puzzle 13 Part B"
+        "Find the fewest number of tokens needed to win prizes in some BIG claw machines."
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        input
+            .split("\n\n")
+            .map(str::trim)
+            .map(ClawMachine::parse)
+            .map(Option::unwrap)
+            .map(|mut m| {
+                m.embiggen();
+                m
+            })
+            .filter_map(|m| m.solve())
+            // button A costs 3, button B costs 1
+            .map(|(a, b)| 3 * a + b)
+            .sum::<i64>()
+            .to_string()
     }
 }
