2b265c9d09aa82701ce35a3c36b957865c500063
Drew Zemke <drew.zemke@gmail.com>
2024-12-15 22:13:50 -0800

    puzzle 15: part b

--- puzzles/15/src/puzzle15b.rs
+++ puzzles/15/src/puzzle15b.rs
@@ -1,13 +1,21 @@
+use crate::{Instructions, WarehouseGrid};
 use common::puzzle::PuzzlePart;
 
 pub struct Puzzle15b {}
 
 impl PuzzlePart for Puzzle15b {
     fn description() -> &'static str {
-        "Puzzle 15 Part B"
+        "Find the coordinates of some boxes after they are pushed around by a robot, but on a horizontally-scaled map."
     }
 
-    fn solve(_input: &str) -> String {
-        "Unsolved!".into()
+    fn solve(input: &str) -> String {
+        let (grid_str, instructions_str) = input.split_once("\n\n").unwrap();
+
+        let mut grid = WarehouseGrid::parse(grid_str);
+        let instructions = Instructions::parse(instructions_str);
+
+        grid.embiggen();
+        grid.move_robot(instructions);
+        grid.box_gps_coord_sum().to_string()
     }
 }
